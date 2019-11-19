caf6e39a0e0f4f30a1bb3f2d673d43d37aea82cc
Diego Righi <diego.righi@paxbr.com.br>
2019-11-19 13:19:26 -0300

    Fazendo conexao com Gateway

--- src/app/routes/gateway.js
+++ src/app/routes/gateway.js
@@ -2,7 +2,8 @@ const express = require('../../config/main-express');
 const api = express.Router();
 const externalHttpRequest = require('../../config/httpRequest');
 const securityIndex = 2;
-const secure = null;
+const removeNewline = require('newline-remove');
+const request = require('request-promise');
 
 //==================================================================
 
@@ -13,7 +14,7 @@ const secure = null;
  */
 async function getSecurityToken(callback){
     try{
-        await externalHttpRequest.get('http://52.168.167.13:1211/v1/getKey/', (error, response, body) => {
+        await externalHttpRequest.get('http://52.168.167.13:1211/v1/getKey/', (error, response) => {
         
             let { publicKey }  = JSON.parse(response.body);
             let security = publicKey.split('-----');
@@ -28,25 +29,39 @@ async function getSecurityToken(callback){
 api.post('/logon', async(req, resp) => {
     let secure;
     getSecurityToken( (resp) => {
-        console.log("MEU TOKEN : "+resp)  
+        let { user } = req.body;
+        let password = removeNewline(resp)
+
+        let requestBody = {
+            'user' : user,
+            'password' : password
+        };
+
+        console.log(password)
+        
+        const options = {
+            method: 'POST',
+            uri: 'http://52.168.167.13:1211/v1/logon/',
+            body: requestBody,
+            json: true,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        request(options).then(function (response){
+            res.status(200).json(response);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+
+
+
     }) 
     
 
-    try{
-        let requestBody = { user, password } = req.body;
-        if(!user && !password) return resp.send({ errorMessage : 'Dados de cadastros insuficientes'});
-
-        await externalHttpRequest.post('http://52.168.167.13:1211/v1/logon/', {
-            requestBody            
-        }, (error, response, body) => {
-            if(error) return resp.send({ errorMessage : 'Ocorreu um erro na autenticação.'}); 
-            
-            console.log(requestBody)
-            return resp.send(response);
-        });
-    }catch(error){
-        return resp.send({ errorMessage : 'Ocorreu um erro com a comunicação do Gateway. Codigo : ROUTES/GATEWAY028-141119' })
-    }
+    
 
 })
 
